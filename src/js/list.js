2ec9fb8aabce5eae19e3323a9791d1f681cf873d
xiongguopan <1603053379@qq.com>
2018-01-08 14:55:02 +0800

    列表页面

--- src/js/list.js
+++ src/js/list.js
@@ -1,11 +1,13 @@
 require(["config"], function(){
 	require(["jquery", "template", "load"], function($, template){
-		异步加载列表页面数据，使用模板引擎渲染
+		//异步加载列表页面数据，使用模板引擎渲染
 		$.getJSON("/mock/list.json", function(data){
 			// 准备渲染数据
 			var renderData = {products : data.res_body.data};
+			console.log(renderData);
 			// 渲染数据
 			var html = template("list_template", renderData);
+			console.log(html);
 			$(".main").html(html);
 		});
 	});
